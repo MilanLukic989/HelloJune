200d5cb315188b0fba89f49bbdf6d0e8c63b5a31
MilanLukic989 <lukic81@gmail.com>
2019-04-18 19:37:23 +0200

    First commit

---
+++ src/milan/Main.java
@@ -0,0 +1,14 @@
+package milan;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+	System.out.println("June was here!");
+	
+	System.out.println("Andrija was here!");
+	
+		
+	}
+
+}
